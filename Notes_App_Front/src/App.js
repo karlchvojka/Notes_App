7d8aba99567316c513f84e82e4ef74275cd1e0a9
Karl Chvojka <karl.chvojka@protonmail.com>
2020-09-25 17:46:21 -0400

    Cleaned up some styles

--- Notes_App_Front/src/App.js
+++ Notes_App_Front/src/App.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react"
 import "./App.scss"
-import APIHelper from "./APIHelper.js"
+import APIHelper from "./helpers/APIHelper.js"
 
 // Component Includes
 import TaskForm from "./components/organisims/TaskForm/"
@@ -19,28 +19,23 @@ function App() {
     } catch (err) {}
   }
 
-  const updateNote = async (e, id) => {
-    e.stopPropagation()
-    const payload = {
-      completed: !notes.find(note => note._id === id).completed,
-    }
-    const updatedNote = await APIHelper.updateNote(id, payload)
-    setNotes(notes.map(note => (note._id === id ? updatedNote : note)))
-  }
-
   const notesLoop = notes.map((note, i) => (
     <NoteItem
       key={note._id}
-      updateNote={updateNote}
       deleteNote={deleteNote}
       note={note}
     />
   ))
 
   return (
-    <div className="App">
-      <TaskForm setNotes={setNotes} currNotes={notes} />
-      <div className="notesList">
+    <div id="App">
+      <header>
+        <h1>Burn_Net: Notes</h1>
+      </header>
+      <div className="formWrap">
+        <TaskForm setNotes={setNotes} currNotes={notes} />
+      </div>
+      <div className="mainWrap">
         <ul>
           {notesLoop}
         </ul>
