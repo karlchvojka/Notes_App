06179366ee10fdeaae7e50e1e6dbb913fc364536
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-05 13:50:35 -0400

    Note item linting

--- Notes_App_Front/src/components/modules/NoteItem/index.js
+++ Notes_App_Front/src/components/modules/NoteItem/index.js
@@ -1,24 +1,24 @@
-import React from "react"
-import "./index.scss"
+import React from 'react';
+import './index.scss';
 
-import CrudHelpers from "../../../helpers/CrudHelpers.js"
+import CrudHelpers from '../../../helpers/CrudHelpers.js';
 
 const NoteItem = (props) => {
-  const { note, setNotes, notes } = props;
+  const { note, notes, setNotes } = props;
 
   const handleClick = (e) => {
-    CrudHelpers.deleteNote(e, note._id)
-    setNotes(notes.filter(({ _id: i }) => note._id !== i))
-  }
+    CrudHelpers.deleteNote(e, note._id);
+    setNotes(notes.filter(({ _id: i }) => note._id !== i));
+  };
 
   return (
     <li
-      key={props.note._id}
-    >
-      {props.note.title}
-      <button className="deleteButton" onClick={e => handleClick(e)}>X</button>
+      key={note._id}
+      >
+      {note.title}
+      <button className="deleteButton" onClick={e => handleClick(e)} type="button">X</button>
     </li>
-  )
-}
+  );
+};
 
-export default NoteItem
+export default NoteItem;
