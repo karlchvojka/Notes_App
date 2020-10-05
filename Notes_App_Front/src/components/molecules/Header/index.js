51df1f4e3f89bb4a15d10730eac55117506801d2
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-05 19:16:14 -0400

    Completed seperating header into its own component

---
+++ Notes_App_Front/src/components/molecules/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+function Header() {
+  return (
+    <header id="mainHeader">
+      <h1>Burn_Net: Notes</h1>
+    </header>
+  );
+}
+
+export default Header;
