7853ecfda1c0710e9b5a9a1ac158b4abaeb6e49b
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-05 21:53:22 -0400

    Implemented router for switching the main area

--- Notes_App_Front/src/App.js
+++ Notes_App_Front/src/App.js
@@ -1,4 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import '../styles/_reset.scss';
 import '../styles/fonts.scss';
 import '../styles/variables.scss';
@@ -8,18 +15,39 @@ import './App.scss';
 import Header from './components/molecules/Header';
 import SideBar from './components/molecules/SideBar';
 import Home from './components/templates/Home';
+import NoteForm from './components/templates/NoteForm';
+
+import APIHelper from './helpers/APIHelper';
 
 function App() {
   const [notes, setNotes] = useState([]);
 
+  const fetchNoteAndSetNotes = async () => {
+    const currNotes = await APIHelper.getAllNotes();
+    setNotes(currNotes);
+  };
+
+  useEffect(() => {
+    fetchNoteAndSetNotes();
+  }, []);
+
   return (
-    <div id="App">
-      <Header />
-      <section className="mainWrap">
-        <SideBar notes={notes} />
-        <Home notes={notes} setNotes={setNotes} />
-      </section>
-    </div>
+    <Router>
+      <div id="App">
+        <header id="mainHeader">
+          <h1>Burn_Net: Notes</h1>
+          <menu>
+            <Link to="/">Home</Link>
+            <Link to="/form">New</Link>
+          </menu>
+        </header>
+        <section className="mainWrap">
+          <SideBar notes={notes} />
+          <Route exact path="/" render={() => <Home notes={notes} setNotes={setNotes} />} />
+          <Route exact path="/form" render={() => <NoteForm notes={notes} setNotes={setNotes} />} />
+        </section>
+      </div>
+    </Router>
   );
 }
 
